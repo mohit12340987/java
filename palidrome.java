3906c9d730d9057122a371414e3633b4aae99d0c
mohit12340987 <tihom0403@gmail.com>
2024-10-07 12:06:40 +0530

    Create palidrome.java

---
+++ palidrome.java
@@ -0,0 +1,20 @@
+class  Palidrome{
+  public static void main(string args[])
+  {
+    String real = "nitin";
+    String rev = "";
+    for(int i = real.length()-1; i>=0;i--)
+    {
+      rev = rev+real;
+    }
+    System.out.Print(rev);
+    if(reav.comparedto(real)==0)
+    {
+      System.out.print("string is palidrome")
+    }
+    else{
+      System.out.print("not")
+    }
+  }
+}
+  
